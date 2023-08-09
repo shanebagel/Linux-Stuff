cc22efccb7ae9e587dab7bc6953f417e038dcc3c
shane <hartley.shane@outlook.com>
2023-08-09 00:39:33 -0400

    Updated file

--- getsysteminfo.sh
+++ getsysteminfo.sh
@@ -12,44 +12,54 @@ getsysteminfo(){
 
 # Run script with getsysteminfo.sh mypc - substitute mypc for your hostname
 
-	if ["$(id -u)" != "0"]; then # If function is ran with sudo - execute script
+
+# Once complete and looking how you want it - remove all the redundant echo commands
+
+	if [ "$(id -u)" = "0" ]; then # If function is ran with sudo - execute script if UID = 0 (Root)
 
 		DATE=$(date)
 		hostname=$1
 
-		echo "Running System Information on $DATE:" > ./systeminfo
-		echo "Hostname:" $(hostname) "has the following uptime:" $(uptime) >> ./systeminfo 
-		uname -a >> ./systeminfo
+		echo "Running System Information on $DATE" > ./systeminfo
+		echo "" >> ./systeminfo
+		echo -e "Hostname:\n" $(hostname) "has the following uptime:" $(uptime) >> ./systeminfo 
+		echo "" >> ./systeminfo
+		echo -e "Kernel Information:\n" $(uname -a) >> ./systeminfo
 		echo "" >> ./systeminfo
 
-		echo "Networking Information:"
-		ip addr show | grep "lo\|inet\|en*\|eth*\|wlan*\|wl*\|vlan*\|veth" | grep -v "inet6\|link/loopback" >> ./systeminfo # Use ip addr to gather networking info, include all loopback, ethernet and wireless adapter info - exclude IPv6
+		echo "Network Information:" >> ./systeminfo
+		Adapters=$(ip addr show)
+		echo "$Adapters" | grep "lo\|inet\|en*\|eth*\|wlan*\|wl*\|vlan*\|veth" | grep -v "inet6\|valid_lft\|link/loopback\|link" >> ./systeminfo # Use ip addr to gather networking info, Include all loopback, ethernet and wireless adapter info, exclude IPv6/MAC
 		echo "" >> ./systeminfo
 
-		echo "Block Storage Devices:"
-		lsblk -f >> ./systeminfo
+		echo "Block Storage Devices:" >> ./systeminfo
+		lsblk -l >> ./systeminfo
 		echo "" >> ./systeminfo
 
-		echo "Disk Utilization:"
-		df -hT | grep -v "tempfs" >> ./systeminfo
+		echo "Disk Utilization:" >> ./systeminfo
+		df -hT | grep -v tmpfs >> ./systeminfo
 		echo "" >> ./systeminfo
 
-		echo "File System Mount Points:"
-		cat /etc/fstab | grep -v "#"
+		echo "File System Mount Points:" >> ./systeminfo
+		cat /etc/fstab | grep -v "#" >> ./systeminfo
 		echo "" >> ./systeminfo
 
-		echo "Firewall Rules:"
+		echo "Firewall Rules:" >> ./systeminfo
 		firewall-cmd --list-all >> ./systeminfo
 		echo "" >> ./systeminfo
 
-		echo "Top Processes Utilizing CPU & Memory Statistics:"
-		ps aux | sort -nrk 3,3 | head >> ./systeminfo # Sort by numeric, sort by CPU utilization, use head to select the top 10 processes using CPU
-		free -th >> /.systeminfo
+		echo -e "Top Processes Utilizing CPU & Memory Statistics:\n" >> ./systeminfo
+		top -o +%CPU -n 1 -b | head -n 18 >> ./systeminfo # Sort by numeric, sort by CPU utilization, use head to select the top 10 processes using CPU 
+		# Top -n: Numer of iterations 1, -b: batch mode, -o: override sort field, select first 10 processes sorted by CPU (including top as a process)
+		echo "" >> ./systeminfo
+		free -th >> ./systeminfo # Free memory - human readable
+
+		printf "Please view the systeminfo file created in: $(pwd -L)\n"
 else	
-	echo "Error - Exit code $?: Script must be ran as administrator - Rerun with Sudo" # Exit with non-zero error
+	echo -e "Error:\n Exit code $?: Script must be ran as administrator - Rerun with Sudo" # Exit with non-zero error
 	exit 1 # Exit with non-zero error
 fi
 
 }
 
-getsysteminfo $@ # Pass in all the arguments made to parameters when the script is ran
+getsysteminfo "$@" # Pass in all the arguments made to parameters when the script is ran
